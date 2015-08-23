5c933210378fac8aef06648c7e685b3262ec2e05
sedighe <bit.mohebbi@gmail.com>
2015-08-23 13:19:26 +0430

    proje java school -1

--- src/pr01/DepositDurationException.java
+++ src/pr01/DepositDurationException.java
@@ -1,8 +1,5 @@
 package pr01;
 
-/**
- * Created by Dotin School1 on 8/10/2015.
- */
 public class DepositDurationException extends  Exception {
     public DepositDurationException(String message) {
         super(message);
